a6a52986c6add66bf14ac69b658c629ced1b189c
Keyvan Alonso <keyvanalonso@gmail.com>
2022-03-17 23:18:04 -0400

    commit tree 72e250c8ec732621deabd1d775681879c47c2d57

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (activity == null) {
+        activity = 'roller-skate';
+    }
+    return (`This Saturday, I want to ${activity}!`)
+}
+
+function mondayWork(activity = "go to the office") {
+    return(`This Monday, I will ${activity}.`);
+}
+
+function wrapAdjective(wrapper= '*'){
+    return function (inner = 'special'){
+        return(`You are ${wrapper}${inner}${wrapper}!`);
+    }
+}
